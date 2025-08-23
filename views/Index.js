a6cb7676a0a4b0b1e51afd6da431345614d7ed46
BASU35 <154794512+BASU35@users.noreply.github.com>
2025-08-24 05:25:34 +0530

    Add files via upload

---
+++ views/Index.js
@@ -0,0 +1,325 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import WaveSurfer from "wavesurfer.js";
+import { FaPlay, FaUndo, FaRedo } from "react-icons/fa";
+import { FaPause } from "react-icons/fa";
+import { useState, useEffect, useRef} from "react";
+// node.js library that concatenates classes (strings)
+import classnames from "classnames";
+// javascipt plugin for creating charts
+import Chart from "chart.js";
+// react plugin used to create charts
+import { Line, Bar } from "react-chartjs-2";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+// core components
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2
+} from "variables/charts.js";
+
+import Header from "components/Headers/Header.js";
+import Player from "components/AudioPlayer.js";
+import disableScroll from "disable-scroll";
+
+const Index = (props) => {
+  disableScroll.off();
+  const [activeNav, setActiveNav] = useState(1);
+  const [chartExample1Data, setChartExample1Data] = useState("data1");
+
+  if (window.Chart) {
+    parseOptions(Chart, chartOptions());
+  }
+
+  const toggleNavs = (e, index) => {
+    e.preventDefault();
+    setActiveNav(index);
+    setChartExample1Data("data" + index);
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const waveformRef = useRef();
+  const trackRef = useRef(); // Separated track playing from waveplayer to support bigger audio files
+  const [waveSurfer, setWaveSurfer] = useState(null); // Holds the reference to created wavesurfer object
+
+  const [playingAudio, setPlayingAudio] = useState(false);
+
+
+  const playAudio = () => {
+    if (!props.hideWave)
+      waveSurfer.play();
+    else
+      trackRef.current.play();
+    setPlayingAudio(true);
+  };
+
+  const pauseAudio = () => {
+    if (!props.hideWave)
+      waveSurfer.pause();
+    else
+      trackRef.current.pause();
+    setPlayingAudio(false);
+  };
+
+
+  useEffect(() => {
+    if (waveformRef.current && trackRef.current && !props.hideWave) {
+      const wavesurfer = props.waveStyles
+        ? WaveSurfer.create({
+          ...props.waveStyles,
+          container: "#waveform",
+          
+          responsive: true,
+          backend: "MediaElement",
+          
+
+        })
+        : WaveSurfer.create({
+          container: "#waveform",
+          responsive: true,
+          backend: "MediaElement"
+        });
+      wavesurfer.setHeight(70);
+      wavesurfer.setProgressColor("#f96332");
+      wavesurfer.setCursorColor("#ffffff00")
+      // Load the waveForm json if provided
+      props.waveJson
+        ? wavesurfer.load(trackRef.current)
+        : wavesurfer.load(trackRef.current, props.waveJson);
+
+      wavesurfer.on("ready", () => {
+        setWaveSurfer(wavesurfer);
+        // Returns the instance to call methods on
+        if (typeof props.getWaveSurferInstance === 'function') {
+          props?.getWaveSurferInstance(waveSurfer)
+        }
+        wavesurfer.zoom(props.zoom);
+      });
+
+      if (props?.events) {
+        Object.entries(props.events).map(([key, value]) => {
+          waveSurfer.on(key, value);
+        })
+      }
+    }
+  }, [
+    props.audioUrl,
+    props.hideWave,
+    props.waveStyles,
+    props.waveJson,
+    props.zoom
+  ]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return (
+    <>
+      {/* <Player
+        audioUrl="https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3"
+        hideImage={true}
+        waveStyles={{
+          cursorWidth: 1,
+          progressColor: "#ee3ec9",
+          responsive: true,
+          waveColor: "#121640",
+          cursorColor: "transparent",
+          barWidth: 0
+        }}
+        {/* zoom={0} */}
+      {/* // waveJson
+      // hideImage="true"
+      // hideWave="true"
+      // containerStyle={}
+      /> */}
+      <Header />
+      {/* Page content */}
+      <Container className="mt--7" fluid>
+        <Row>
+          <Col className="mb-5 mb-xl-0" xl="8">
+            <Card className="bg-gradient-default shadow">
+              <CardHeader className="bg-transparent">
+                <Row className="align-items-center">
+                  <div className="col">
+                    <h6 className="text-uppercase text-light ls-1 mb-1">
+                      Overview
+                    </h6>
+                    <h2 className="text-white mb-0">Heart Health</h2>
+                  </div>
+                  <div className="col">
+                    <Nav className="justify-content-end" pills>
+                      <NavItem>
+                        <NavLink
+                          className={classnames("py-2 px-3", {
+                            active: activeNav === 1
+                          })}
+                          href="#pablo"
+                          onClick={(e) => toggleNavs(e, 1)}
+                        >
+                          <span className="d-none d-md-block">Month</span>
+                          <span className="d-md-none">M</span>
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          className={classnames("py-2 px-3", {
+                            active: activeNav === 2
+                          })}
+                          data-toggle="tab"
+                          href="#pablo"
+                          onClick={(e) => toggleNavs(e, 2)}
+                        >
+                          <span className="d-none d-md-block">Week</span>
+                          <span className="d-md-none">W</span>
+                        </NavLink>
+                      </NavItem>
+                    </Nav>
+                  </div>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                {/* Chart */}
+                <div className="chart">
+                  <Line
+                    data={chartExample1[chartExample1Data]}
+                    options={chartExample1.options}
+                    getDatasetAtEvent={(e) => console.log(e)}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xl="4">
+            <Card className="shadow">
+              <CardHeader className="bg-transparent">
+                <Row className="align-items-center">
+                  <div className="col">
+                    <h6 className="text-uppercase text-muted ls-1 mb-1">
+                      Performance
+                    </h6>
+                    <h2 className="mb-0">Average Heart Rate</h2>
+                  </div>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                {/* Chart */}
+                <div className="chart">
+                  <Bar
+                    data={chartExample2.data}
+                    options={chartExample2.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col className="mb-5 mb-xl-0" xl="12">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <Row className="align-items-center">
+                  <div className="col">
+                    <h3 className="mb-0">Recent Recordings</h3>
+                  </div>
+                  <div className="col text-right">
+                    <Button
+                      color="primary"
+                      href="/admin/recordings"
+                      // onClick={(e) => e.preventDefault()}
+                      size="sm"
+                    >
+                      See all
+                    </Button>
+                  </div>
+                </Row>
+              </CardHeader>
+              <Table className="align-items-center " responsive>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">Recording</th>
+                    <th scope="col">{" "}</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Classification</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <Player audioUrl="https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201102270940.wav" health={"Normal"} date={23} />
+                  <Player audioUrl="https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201101070538.wav" health={"Normal"} date={23} />
+                  <Player audioUrl="https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201101151127.wav" health={"Normal"} date={23} />
+                  <Player audioUrl="https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201102081152.wav" health={"Normal"} date={23} />
+                  
+                </tbody>
+              </Table>
+            </Card>
+          </Col>
+          <Col xl="4">
+            
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Index;
---
+++ views/examples/Tables.js
@@ -0,0 +1,307 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// reactstrap components
+import Player from "../../components/AudioPlayer.js";
+import {
+  Badge,
+  Card,
+  CardHeader,
+  CardFooter,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Media,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Progress,
+  Table,
+  Container,
+  Row,
+  UncontrolledTooltip
+} from "reactstrap";
+
+// core components
+import Header from "components/Headers/Header.js";
+import {
+  Button,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  Col
+} from "reactstrap";
+import { useState } from "react";
+import { Uploader } from "uploader";
+import { UploadButton } from "react-uploader";
+import { LoopCircleLoading } from "react-loadingg";
+import { useEffect } from 'react';
+import { data } from "jquery";
+import disableScroll from 'disable-scroll';
+const Tables = () => {
+  disableScroll.on()
+  // const [list, setList] = useState("");
+  // const [uploadded, setUploaded] = useState(false);
+  // const [murmur, setmurmur] = useState(false)
+  // const [normal, setnormal] = useState(false)
+  // const [artifact, setArtifact] = useState(false)
+  // const [extrahls, setExtrahls] = useState(false)
+  // const [artifactUrl, setartifactUrl] = useState("")//do same for other conditions
+  // const [murmurUrl, setmurmurUrl] = useState("")
+  // const [normalUrl, setnormalUrl] = useState("") 
+  // const [extrahlsUrl, setextrahlsUrl] = useState("")
+  //   var newData=[];
+    const [data, setdata] = useState([
+        { url: "https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201102270940.wav", health: "Normal", date: 23 },
+        { url: "https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201101070538.wav", health: "Normal", date: 23 },
+        { url: "https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201101151127.wav", health: "Normal", date: 23 },
+        { url: "https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201102081152.wav", health: "Normal", date: 23 },
+        { url: "https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201103140132.wav", health: "Normal", date: 22 },
+        { url: "https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201103140135.wav", health: "Normal", date: 21 },
+        { url: "https://raw.githubusercontent.com/aarush-dev/argon-dashboard-react/0cf01dca4fd8db9886f4d338420c1dc98b56ef5c/archive(1)/set_a/normal__201103140822.wav", health: "Normal", date: 20 }
+    ])
+
+  const uploader = new Uploader({
+    // Get production API keys from Upload.io
+    apiKey: "public_G22nhtq4iGw67xZ7TzW95Ay2SZfv"
+  });
+  // get the first letter of the file uploaded
+  const uploadHandler = (files) => {
+    console.log(files);
+    console.log(files[0].originalFile.file.name.charAt(0));
+    // setList(files[0].fileUrl);
+    if (files[0].originalFile.file.name.charAt(0) == "a" || files[0].originalFile.file.name.charAt(0) == "A") { 
+      setdata([{ url: files[0].fileUrl, health: "Artifact", date: 23 },...data]);
+      console.log(data);
+      ;
+    }//do same for other conditions
+    else if (files[0].originalFile.file.name.charAt(0) == "m" || files[0].originalFile.file.name.charAt(0) == "M") {
+      setdata([{ url: files[0].fileUrl, health: "Murmur", date: 23 },...data]);
+      console.log(data);
+      ;
+    }
+    else if (files[0].originalFile.file.name.charAt(0) == "n" || files[0].originalFile.file.name.charAt(0) == "N" || files[0].originalFile.file.name.charAt(0) == "R" || files[0].originalFile.file.name.charAt(0) == "r") {
+      setdata([{ url: files[0].fileUrl, health: "Normal", date: 23 },...data]);
+      console.log(data);
+      ;
+    }
+    else if (files[0].originalFile.file.name.charAt(0) == "e" || files[0].originalFile.file.name.charAt(0) == "E") {
+      setdata([{ url: files[0].fileUrl, health: "ExtraHLS", date: 23 },...data]);
+      console.log(data);
+      ;
+    }
+    else {
+      alert("Please upload a valid file");
+    }
+    // if (files[0].originalFile.file.name.charAt(0) == "m" || files[0].originalFile.file.name.charAt(0) == "M"){setmurmur(true);setmurmurUrl(files[0].fileUrl)}
+    // if (files[0].originalFile.file.name.charAt(0) == "e" || files[0].originalFile.file.name.charAt(0) == "E"){setExtrahls(true);setextrahlsUrl(files[0].fileUrl)}
+    // if (files[0].originalFile.file.name.charAt(0) == "n" || files[0].originalFile.file.name.charAt(0) == "N"){setnormal(true);setnormalUrl(files[0].fileUrl)}
+    // console.log(uploadded+" "+murmur+" "+normal+" "+artifact+" "+extrahls)
+
+    // [
+    //   {
+    //     "originalFile": {
+    //       "accountId": "FW25avz",
+    //       "file": {},
+    //       "fileUrl": "https://upcdn.io/FW25avz/raw/uploads/2022/10/09/4482_3944feb-3sRg.pdf",
+    //       "filePath": "/uploads/2022/10/09/4482_3944feb-3sRg.pdf",
+    //       "size": 3065,
+    //       "mime": "application/pdf",
+    //       
+    //       "tags": [
+    //         "timestamp/2022-10-09/20.34.08.453Z",
+    //         "mime/application/pdf",
+    //         "origin/http/localhost/3000",
+    //         "ip/122.177.107.232",
+    //         "api_key/public/FW25a...P533G",
+    //         "unassigned"
+    //       ],
+    //       "lastModified": 1665347648453,
+    //       "originalFileName": "4482_3944feb.pdf"
+    //     },
+    //     "fileUrl": "https://upcdn.io/FW25avz/raw/uploads/2022/10/09/4482_3944feb-3sRg.pdf"
+    //   }
+    // ]
+  }
+
+
+
+
+
+
+  // data={
+  //   url:""
+  // }
+  // const normalHeart=()=>{
+  //   if (normal){
+  //     return(
+  //     <Player audioUrl={normalUrl} health={"Normal"} date={10} />
+  //   )}
+  //   }//create similar functions for other 3 conditions
+  // const murmurHeart=()=>{
+  //   if (murmur){
+  //     return(
+  //     <Player audioUrl={murmurUrl} health={"Murmur"} date={10} />
+  //   )}
+  //   }
+  // const artifactHeart=()=>{
+  //   if (artifact){
+  //     return(
+  //     <Player audioUrl={artifactUrl} health={"Artifact"} date={10} />
+  //   )}
+  //   }
+  // const extrahlsHeart=()=>{
+  //   if (extrahls){
+  //     return(
+  //     <Player audioUrl={extrahlsUrl} health={"ExtraHLS"} date={10} />
+  //   )}
+  //   }
+
+
+  return (
+    <div className="overflow-hidden">
+
+      <Header />
+      {/* Page content */}
+      <Container className="mt--7" fluid>
+        {/* Table */}
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <Row className="align-items-center">
+                  <div className="col">
+                    <h3 className="mb-0">Recordings</h3>
+                  </div>
+                  <div className="col text-right">
+
+                  </div>
+                  <div className="col">
+                    <div className="col text-right overflow-hidden">
+                      
+                          {/* <Button
+                            color="primary"
+                            // onClick={(e) => {}
+                            onClick={(e) => e.preventDefault()}
+                            size="md"
+                          > Record
+                          </Button> */}
+
+                      <UploadButton uploader={uploader}
+                        options={{ multi: true }}
+                        onComplete={files => { uploadHandler(files) }}>
+                        {({ onClick }) =>
+                          <Button
+                            color="primary"
+                            onClick={onClick}
+                            // onClick={(e) => e.preventDefault()}
+                            size="md"
+                          > Upload
+                          </Button>
+
+                        }
+                      </UploadButton>
+
+
+                    </div>
+                  </div>
+                </Row>
+
+              </CardHeader>
+              <Table className="align-items-center " responsive>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">Recording</th>
+                    <th scope="col">{" "}</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Classification</th>
+                  </tr>
+                </thead>
+                <tbody className="overscroll-none overflow-hidden">
+                  {/* TODO:*/}
+                  {data.slice(0, 7).map((item)=>{
+                    return(
+                      <Player audioUrl={item.url} health={item.health} key={item.url} date={item.date} />
+                    )
+                    
+                  })}
+                </tbody>
+              </Table>
+              <CardFooter className="py-4">
+                <nav aria-label="...">
+                  <Pagination
+                    className="pagination justify-content-end mb-0"
+                    listClassName="justify-content-end mb-0"
+                  >
+                    <PaginationItem className="enabled">
+                      <PaginationLink
+                        href=""
+                        onClick={(e) => e.preventDefault()}
+                        tabIndex="-1"
+                      >
+                        <i className="fas fa-angle-left" />
+                        <span className="sr-only">Previous</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem className="active">
+                      <PaginationLink
+                        href=""
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        1
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href=""
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        2 <span className="sr-only">(current)</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href=""
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        3
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href=""
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className="fas fa-angle-right" />
+                        <span className="sr-only">Next</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                  </Pagination>
+                </nav>
+              </CardFooter>
+            </Card>
+          </div>
+        </Row>
+
+      </Container>
+    </div>
+  );
+};
+
+export default Tables;
