5fe09ef0935f94dee06919bd35d3ffbeaec86016
BASU35 <154794512+BASU35@users.noreply.github.com>
2025-08-24 05:42:33 +0530

    Update VoiceRecorder.js

--- components/VoiceRecorder.js
+++ components/VoiceRecorder.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// Import Audio Recorder
 import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
 import 'audio-react-recorder/dist/index.css'
 
@@ -7,30 +8,36 @@ class App extends React.Component {
     constructor(props) {
         super(props)
 
+        // Initial state
         this.state = {
-            recordState: null,
-            audioData: null
+            recordState: null, // current recording state (START, PAUSE, STOP)
+            audioData: null    // recorded audio data (Blob + URL)
         }
     }
 
+    // Start recording
     start = () => {
         this.setState({
             recordState: RecordState.START
         })
     }
 
+    // Pause recording
     pause = () => {
         this.setState({
             recordState: RecordState.PAUSE
         })
     }
 
+    // Stop recording
     stop = () => {
         this.setState({
             recordState: RecordState.STOP
         })
     }
 
+    // Called automatically when recording is stopped
+    // "data" contains Blob + URL of recorded audio
     onStop = (data) => {
         this.setState({
             audioData: data
@@ -43,28 +50,19 @@ class App extends React.Component {
 
         return (
             <div>
+                {/* Audio waveform + recording logic */}
                 <AudioReactRecorder
                     state={recordState}
                     onStop={this.onStop}
-                    backgroundColor='rgb(255,255,255)'
+                    backgroundColor='rgb(255,255,255)' // White background
                 />
+
+                {/* Audio playback once recording is done */}
                 <audio
                     id='audio'
                     controls
                     src={this.state.audioData ? this.state.audioData.url : null}
                 ></audio>
-                <button id='record' onClick={this.start}>
-                    Start
-                </button>
-                <button id='pause' onClick={this.pause}>
-                    Pause
-                </button>
-                <button id='stop' onClick={this.stop}>
-                    Stop
-                </button>
-            </div>
-        )
-    }
-}
 
-export default App
+                {/* Recording controls */}
+                <button
