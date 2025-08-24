2114b1a56704acc244c584af28bdf8bce3803f11
BASU35 <154794512+BASU35@users.noreply.github.com>
2025-08-24 05:39:30 +0530

    Update AuthNavbar.js

--- components/Navbars/AuthNavbar.js
+++ components/Navbars/AuthNavbar.js
@@ -1,22 +1,21 @@
 /*!
 
 =========================================================
-* Argon Dashboard React - v1.2.2
+* Dashboard React - v1.2.2
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/argon-dashboard-react
 * Copyright 2022 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
 
-* Coded by Creative Tim
-
 =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+
 import { Link } from "react-router-dom";
-// reactstrap components
+// Importing layout and navigation components from reactstrap
 import {
   UncontrolledCollapse,
   NavbarBrand,
@@ -29,11 +28,21 @@ import {
   Col
 } from "reactstrap";
 
+// AdminNavbar Component
+// ----------------------
+// This is a placeholder for the admin navigation bar.
+// It currently renders nothing but can later include:
+//   - Branding / logo
+//   - Navigation links
+//   - Collapsible menu for mobile devices
+//   - User actions (profile, logout, etc.)
 const AdminNavbar = () => {
   return (
     <>
+      {/* Placeholder for navbar content */}
     </>
   );
 };
 
+// Exporting AdminNavbar for use in other parts of the application
 export default AdminNavbar;
