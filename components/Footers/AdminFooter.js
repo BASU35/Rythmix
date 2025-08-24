fddd5b3bccdfbaaa55406b0fa754c23932888de8
BASU35 <154794512+BASU35@users.noreply.github.com>
2025-08-24 05:33:05 +0530

    Update AdminFooter.js

--- components/Footers/AdminFooter.js
+++ components/Footers/AdminFooter.js
@@ -1,6 +1,40 @@
 /*!
 
 =========================================================
+* Dashboard React - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+
+// Importing layout and navigation components from reactstrap library
+import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
+
+// Footer Component
+// ----------------
+// This component renders the footer section of the dashboard.
+// Currently, it only adds vertical padding but can be expanded later
+// to include links, credits, or other footer content.
+const Footer = () => {
+  return (
+    // Wrapper div with padding applied at the top
+    <div className="pt-8"></div>
+  );
+};
+
+// Exporting Footer so it can be used in other parts of the application
+export default Footer;
+/*!
+
+=========================================================
 * Argon Dashboard React - v1.2.2
 =========================================================
 
