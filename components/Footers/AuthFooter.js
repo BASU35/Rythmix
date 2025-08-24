92b6e56c68d9320eb75091181cb636c817bbb99c
BASU35 <154794512+BASU35@users.noreply.github.com>
2025-08-24 05:36:54 +0530

    Update AuthFooter.js

--- components/Footers/AuthFooter.js
+++ components/Footers/AuthFooter.js
@@ -8,7 +8,6 @@
 * Copyright 2022 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
 
-* Coded by Creative Tim
 =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
