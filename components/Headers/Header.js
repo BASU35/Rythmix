8477d0b6b349f8c211281e7289be6bb98695a34e
BASU35 <154794512+BASU35@users.noreply.github.com>
2025-08-24 05:34:45 +0530

    Update Header.js

--- components/Headers/Header.js
+++ components/Headers/Header.js
@@ -1,40 +1,50 @@
 /*!
 
 =========================================================
-* Argon Dashboard React - v1.2.2
+* Dashboard React - v1.2.2
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/argon-dashboard-react
 * Copyright 2022 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/
-
-
-
-
--dashboard-react/blob/master/LICENSE.md)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
-
 =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
 
-// reactstrap components
+// Importing UI components from reactstrap
 import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
 
+// Header Component
+// ----------------
+// This component is responsible for rendering the top section
+// of the dashboard interface. It provides space for:
+//   - Page titles
+//   - Statistics cards
+//   - Action buttons or summaries
+//   - Gradient background styling for a modern look
+//
+// Currently, it only renders the container with the background
+// and reserved space. You can extend it to include cards, charts,
+// or quick-access information.
 const Header = () => {
   return (
     <>
+      {/* Header with gradient background and padding */}
       <div className="header bg-gradient-info pb-6 pt-5 pt-md-8">
         <Container fluid>
           <div className="header-body">
-           </div>
+            {/* Placeholder for statistics cards, titles, or actions */}
+          </div>
         </Container>
       </div>
     </>
   );
 };
 
+// Exporting Header so it can be used across the application
 export default Header;
+
